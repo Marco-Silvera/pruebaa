5208c266a1d26553aea003237007ad2fd26b1ba5
Marco Silvera <msilveradev@gmail.com>
2024-10-16 20:03:49 -0500

    actualizacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
     adapter: vercel(),
-    output:"server",
     integrations: [tailwind(), react()],
 });
